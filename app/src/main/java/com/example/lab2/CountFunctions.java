1e7a009f061d95ee7416190fb4801623aa73e929
Xasthul <nazargon2003@gmail.com>
2022-09-13 22:30:00 +0300

    Added file with count functions

---
+++ app/src/main/java/com/example/lab2/CountFunctions.java
@@ -0,0 +1,13 @@
+package com.example.lab2;
+
+public class CountFunctions {
+    static int CountWords(String text){
+        String words[] = text.split("[ .,]+");
+        return words.length;
+    }
+
+    static int CountChars(String text){
+        String chars[] = text.split("\\s");
+        return text.length();
+    }
+}
