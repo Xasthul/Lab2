b2e2c9529abcaa78d4e61cefb95bb0ee909b151f
Xasthul <nazargon2003@gmail.com>
2022-10-19 11:25:26 +0300

    Added Unit Tests and UI Test

--- app/src/main/java/com/example/lab2/CountFunctions.java
+++ app/src/main/java/com/example/lab2/CountFunctions.java
@@ -1,13 +1,18 @@
 package com.example.lab2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CountFunctions {
     static int countWords(String text){
-        String[] words = text.split("[ .,]+");
-        return words.length;
+        ArrayList<String> words = new ArrayList<String>(Arrays.asList(text.split("[ .,]+")));
+        if (words.size() > 0 && words.get(0).equals("")) {
+            words.remove(0);
+        }
+        return words.size();
     }
 
     static int countChars(String text){
-        String[] chars = text.split("\\s");
         return text.length();
     }
 }
